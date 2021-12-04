5d12ff052ae522923d3334eec8e4a5f93517b4c9
John Lockwood <elitepropertiesbroker@gmail.com>
2021-12-04 11:15:14 -0500

    pytest example

---
+++ src/greeter.py
@@ -0,0 +1,9 @@
+"""greeter module contains one testable function"""
+
+
+def greet(name: str = None) -> str:
+    """greet is a hello world style function to have something to test."""
+    if name is None:
+        return "Hello, World!"
+    else:
+        return f"Hello, {name}!"
---
+++ test/test_greeter.py
@@ -0,0 +1,9 @@
+"""tests the src.greeter module"""
+
+from src.greeter import greet
+
+def test_greeter_returns_hello_world_if_no_name_provided():
+    assert greet() == "Hello, World!"
+
+def test_greeter_returns_hello_name_if_name_provided():
+    assert greet("John") == "Hello, John!"
