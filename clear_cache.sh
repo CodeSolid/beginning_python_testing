57c1a367d923c3599e3ae221a55bade618b2989b
John Lockwood <elitepropertiesbroker@gmail.com>
2022-01-27 16:07:31 -0500

    clear_cache.sh fix

--- clear_cache.sh
+++ clear_cache.sh
@@ -1,2 +1,2 @@
 # Use this to clear the python cache files if tests won't refresh
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
+find . | grep -E "(.pytest_cache|__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
