021c3fe1c2ed59c9ee730fad50b949dc51e30d02
John Lockwood <elitepropertiesbroker@gmail.com>
2022-01-27 10:58:44 -0500

    Rework source structure to recommended

--- test/test_greeter.py
+++ test/test_greeter.py
@@ -1,6 +1,6 @@
-"""tests the src.greeter module"""
+"""tests the myapp.greeter module"""
 
-from src.greeter import greet
+from myapp.greeter import greet
 
 def test_greeter_returns_hello_world_if_no_name_provided():
     assert greet() == "Hello, World!"
